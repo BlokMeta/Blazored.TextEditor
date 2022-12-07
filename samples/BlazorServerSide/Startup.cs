6e72fc90eb344ff0027989f7cfc9ebc7e28f0aef
Ayberk Kolcu <ayberkkolcu@gmail.com>
2022-12-07 09:50:13 +0000

    Lifetime to SWingleton!

--- samples/BlazorServerSide/Startup.cs
+++ samples/BlazorServerSide/Startup.cs
@@ -28,7 +28,7 @@ namespace BlazorServerSide
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<EmailDto>();
+            services.AddSingleton<EmailDto>();
             services.AddDbContext<EmailDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddRazorPages();
             services.AddServerSideBlazor().AddCircuitOptions(options => { options.DetailedErrors = true; });
